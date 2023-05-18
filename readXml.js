4e607db50f0d0ae293573f54a9e7de3971f89514
Milos1988 <vojvoda1988@gmail.com>
2023-05-18 10:48:47 +0200

    Extracted name and surname

--- readXml.js
+++ readXml.js
@@ -1,40 +1,16 @@
-// const fs = require('fs');
-// const parser = require('fast-xml-parser')
-
-// const xmlData = fs.readFileSync('/home/milos/Downloads/file.xml', 'utf-8');
-
-// const parser = new xml2js.Parser();
-// parser.parseString(xmlData, (err, result) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     // Dobijeni rezultat je JavaScript objekat koji sadrži XML strukturu
-//     console.log(result['ns1:PodaciPoreskeDeklaracije'])
-//   }
-// });
-
 const fs = require('fs');
-const parser = require('fast-xml-parser');
+const xml2js = require('xml2js');
 
-// Putanja do XML dokumenta
-const filePath = '/home/milos/Downloads/file.xml';
+const xml = fs.readFileSync('C:/Users/Client85a/Downloads/doc.xml', 'utf-8');
 
-// Opcije parsiranja
-const options = {
-  attributeNamePrefix: '',
-  ignoreAttributes: false,
-  parseAttributeValue: true
-};
-
-// Pročitajte sadržaj XML dokumenta
-fs.readFile(filePath, 'utf-8', (err, data) => {
+xml2js.parseString(xml, (err, result) => {
   if (err) {
-    console.error('Greška prilikom čitanja XML dokumenta:', err);
+    console.error(err);
     return;
   }
 
-  // Parsirajte XML podatke
-  const jsonObj = parser.parse(data, options);
+  const name = result['ns1:PodaciPoreskeDeklaracije']['ns1:DeklarisaniPrihodi'][0]['ns1:PodaciOPrihodima']
+    .map((income) => income['ns1:Ime'][0] + ' ' + income['ns1:Prezime'][0]);
 
-  console.log(jsonObj); // Prikazuje parsirane XML podatke kao JavaScript objekat
+  console.log(name);
 });
